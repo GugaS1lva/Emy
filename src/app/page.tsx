0c5b9590673ea8206eb33d6a5c98bdf8a49cb881
Gustavo Silva <contatogustavosilva017@outlook.com>
2025-03-14 07:29:28 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="pt-[80px] flex flex-col items-center justify-center w-full h-screen text-7xl font-bold bg-linear-to-r from-rose-500 via-pink-500 to-red-500">
+    <main className="pt-[80px] flex flex-col items-center justify-center w-full h-screen font-bold bg-linear-to-r from-rose-500 via-pink-500 to-red-500">
 
       <div className=" flex flex-col items-center">
         <Image
           src={'/emy.webp'}
-          width={300}
-          height={300}
+          width={250}
+          height={1}
           alt="emy"
         />
-        <h1>Pack do pé da emy</h1>
+        <h1 className="text-4xl">Pack do pé da emy ♡</h1>
         <Image
           src={'/emy.webp'}
-          width={300}
-          height={300}
+          width={250}
+          height={1}
           alt="emy"
         />
       </div>
 
-      <section className="text-2xl">Capa das 21 linhas (cm)</section>
+      <section className="text-2xl">Capa pras 21 linhas (cm) 🍆</section>
     </main>
   );
 }
